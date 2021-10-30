d22e22a2faa29d1567eaba8a5bda7436cfccd0f0
AleksandrFROLik <nextfrolik@gmail.com>
2021-10-30 23:26:51 +0300

    connected SuperButton and SuperInput

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
 import style from './Button.module.css'
 import {FilterValuesType} from "../../App";
-type ButtonType = {
+
+
+type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+type ButtonType = DefaultButtonPropsType & {
     name: string
     callBack: () => void
     filter?: FilterValuesType
 }
 
-export const Button = ({name, callBack, filter}: ButtonType) => {
+export const Button: React.FC<ButtonType> = (
+    {
+        name, callBack, filter,
+        ...restProps
+    }
+) => {
+
     const onClickHandler = () => {
         callBack()
     }
     return (
         <button onClick={onClickHandler} className={filter === name ? `${style.activeFilter}` : ''}>{name}</button>
+
     )
 }
+

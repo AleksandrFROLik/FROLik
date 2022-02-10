1bcdc97011924f26d18556e4f9136818ea98f78f
AleksandrFROLik <nextfrolik@gmail.com>
2022-02-10 21:20:19 +0300

    refactoring code in task-api.ts and todolists-api.ts +

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -11,3 +11,7 @@ export const handleServerAppError = <T>(dispatch: Dispatch<AppActionsType>, data
     dispatch(setAppStatus("failed"))
     dispatch(setAppErrorAC(data.messages.length ? data.messages[0] : 'Some error'))
 }
+
+export const deleteInfo = ()=> {
+
+}

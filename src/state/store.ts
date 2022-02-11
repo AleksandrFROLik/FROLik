08483628fcd310b54c10684aef0a9bda0e85c523
AleksandrFROLik <nextfrolik@gmail.com>
2022-02-11 23:32:59 +0300

    add thunkType+

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,7 +2,7 @@ import {TasksActionsType, tasksReducer} from './tasks-reducer';
 import {TodolistsActionsType, todolistsReducer} from './todolists-reducer';
 import {applyMiddleware, combineReducers, createStore} from 'redux';
 import thunk, {ThunkAction} from "redux-thunk";
-import {appReducer} from "./app-reducer";
+import {AppActionsType, appReducer} from "./app-reducer";
 import {TypedUseSelectorHook, useSelector} from "react-redux";
 
 const rootReducer = combineReducers({
@@ -14,7 +14,7 @@ const rootReducer = combineReducers({
 export const store = createStore(rootReducer, applyMiddleware(thunk));
 export type AppRootStateType = ReturnType<typeof rootReducer>
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
-export type AppRootReducerActionType = TasksActionsType |TodolistsActionsType
+export type AppRootReducerActionType = TasksActionsType |TodolistsActionsType | AppActionsType
 export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppRootReducerActionType>
 
 // @ts-ignore

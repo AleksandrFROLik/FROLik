9402f5c4452695f8ce2bae7e96638ce32a9e93ff
AleksandrFROLik <nextfrolik@gmail.com>
2022-02-25 14:12:38 +0300

    add useFormik and component Error

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React, {useEffect} from 'react'
-import {useDispatch} from 'react-redux';
+import {Navigate, Route, Routes} from "react-router-dom";
 
+import {useDispatch} from 'react-redux';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-import Menu from '@mui/icons-material/Menu';
 import LinearProgress from '@mui/material/LinearProgress';
+import Menu from '@mui/icons-material/Menu';
 
 import './App.css';
 import {getTodoListsTC} from './features/TodolistLists/todolists-reducer';
 import {useAppSelector} from './state/store';
 import {RequestStatusType} from "./state/app-reducer";
-import {ErrorSnackbar} from "./components/errorSnackbar/ErrorSnackbar";
 import {TodolistLists} from "./features/TodolistLists/TodolistLists";
+import {Login} from "./features/Login/Login";
+import {Error} from "./features/Error/Error";
+import {ErrorSnackbar} from "./components/errorSnackbar/ErrorSnackbar";
 
 
 function App() {
@@ -43,7 +46,13 @@ function App() {
             {status === 'loading' && <LinearProgress/>}
 
             <Container fixed>
-                <TodolistLists/>
+                <Routes>
+                    <Route path='/' element={<TodolistLists/>}/>
+                    <Route path='login' element={<Login/>}/>
+                    <Route path='404' element={<Error/>}/>
+                    <Route path='*' element={<Navigate to='404'/>}/>
+                </Routes>
+
             </Container>
             <ErrorSnackbar/>
         </div>
---
+++ src/features/Error/Error.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Error = () => {
+    return (
+       <h1 style={{textAlign:'center', color:'red', fontSize:'28'}}>Page not found. Error 404</h1>
+    );
+};
+
